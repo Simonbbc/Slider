1dedc2b35ea3ac041ae71ada16c849e232ced157
simon <simon.bock@liip.ch>
2019-04-10 14:34:54 +0200

    activate effect and deactivate effect buttons work

--- Js_Slider_Project/assets/js/main.js
+++ Js_Slider_Project/assets/js/main.js
@@ -1,31 +1,28 @@
 'use strict';
 let currentPosX = 0;
-//index 0 = left swipe
-//index 1 = right swipe
+let isEffect = false;
 const imageTrack = document.querySelector(".slider__track");
-//const images = document.querySelectorAll(".slider__image");
-//const newImages = images.cloneNode(true);
-//const newImageTrack = imageTrack.cloneNode(true);
-//document.querySelector(".slider__container").appendChild(newImageTrack);
-//console.log(newImages);
-function cloneImages() {
-    document.querySelectorAll(".slider__image").forEach((element, index) => {
-        const newImage = element.cloneNode(true);
-        imageTrack.removeChild(element);    
-        //imageTrack.appendChild(newImage);
-        
-    });
-}
 
-
-document.querySelectorAll(".button__swipe").forEach((element, index) => {
-    element.addEventListener('click', function () {
+document.querySelectorAll(".button__swipe").forEach((swiperButton, index) => {
+    swiperButton.addEventListener('click', function () {
         switch(index) {
             case 1:
-                imageTrack.style.transform = `translateX(${goRight()}px)`;
+                if(isEffect) {
+                    imageTrack.style.transform = `translateX(${goRight()}px)`;
+                    imageTrack.style.transition = `transform 0.8s ease 0s`;
+                } else if (!isEffect) {
+                    imageTrack.style.transform = `translateX(${goRight()}px)`;
+                    imageTrack.style.transition = `transform 0s ease 0s`;
+                }
                 break;
             case 0:
-                imageTrack.style.transform = `translateX(${goLeft()}px)`;
+                if(isEffect) {
+                    imageTrack.style.transform = `translateX(${goLeft()}px)`;
+                    imageTrack.style.transition = `transform 0.8s ease 0s`;
+                } else if (!isEffect) {
+                    imageTrack.style.transform = `translateX(${goLeft()}px)`;
+                    imageTrack.style.transition = `transform 0s ease 0s`;
+                }
                 break;
             default:
                 imageTrack.style.transform = 'translateX(0px)';
@@ -36,14 +33,53 @@ document.querySelectorAll(".button__swipe").forEach((element, index) => {
 
 function goRight() {
     if(document.querySelector('.slider__container').getBoundingClientRect().left + document.querySelector('.slider__container').clientWidth === document.querySelector('.slider__track').getBoundingClientRect().right) {
-        cloneImages();       
+        return currentPosX;      
     }
     return currentPosX -= 500;
 }
 
 function goLeft() {
     if(document.querySelector('.slider__container').getBoundingClientRect().left === document.querySelector('.slider__track').getBoundingClientRect().left) {
-        cloneImages();
+        return currentPosX;
     }
     return currentPosX += 500;
 }
+
+
+document.querySelectorAll(".button--effect").forEach((effectButton, index) => {
+    effectButton.addEventListener("click", function() {
+        switch(index) {
+            case 0:
+                isEffect = true;
+                console.log("effect yes: ", isEffect)
+                break;
+            case 1:
+                isEffect = false;
+                console.log("effect no: ", isEffect)
+                break;
+            default:
+                isEffect = false;
+                break;
+        }
+    });
+});
+
+
+/**
+ * Tests wie CloneNode() Funktion angewendet wird.
+index 0 = left swipe
+index 1 = right swipe
+const images = document.querySelectorAll(".slider__image");
+const newImages = images.cloneNode(true);
+const newImageTrack = imageTrack.cloneNode(true);
+document.querySelector(".slider__container").appendChild(newImageTrack);
+console.log(newImages);
+function cloneImages() {
+    document.querySelectorAll(".slider__image").forEach((element, index) => {
+        // const newImage = element.cloneNode(true);
+        // imageTrack.removeChild(element);    
+        //imageTrack.appendChild(newImage);
+        
+    });
+} 
+ */
