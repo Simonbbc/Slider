946733bd318bcf8ebaaa9ace0459a06c8fa4645b
simon <simon.bock@liip.ch>
2019-04-30 16:24:02 +0200

    add fade effect for buttons and keyboard

--- Js_Slider_Project/assets/js/main.js
+++ Js_Slider_Project/assets/js/main.js
@@ -1,9 +1,11 @@
 'use strict';
 
 let currentPosX = 0;
-let isEffect = false;
+let effect = "";
+let imageIndex = 0;
 const imageTrack = document.querySelector(".slider__track");
 const imageContainer = document.querySelector(".slider__container");
+const images = document.querySelectorAll(".slider__image");
 /**
  * Zuerst holen wir uns die Beiden Knöpfe, welche da sind um im Slider zu navigieren (button__swipe).
  * Danach binden wir den Knöpfen einen Click Listener an.
@@ -18,21 +20,45 @@ document.querySelectorAll(".button__swipe").forEach((swiperButton, index) => {
     swiperButton.addEventListener('click', function () {
         switch(index) {
             case 1:
-                if(isEffect) {
+                if(effect.includes("slide")) {
                     imageTrack.style.transform = `translateX(${goRight()}px)`;
                     imageTrack.style.transition = `transform 0.8s ease 0s`;
-                } else if (!isEffect) {
+                    imageIndex += 1;
+                } else if (effect.includes("fade")) {
+                    if(imageIndex < 4) {
+                        images[imageIndex].classList.toggle("slider__image--fade");
+                        window.setTimeout(function () {
+                            imageTrack.style.transform = `translateX(${goRight()}px)`;
+                            imageTrack.style.transition = `transform 0s ease 0s`;
+                            imageIndex += 1;
+                            images[imageIndex].classList.toggle("slider__image--fade");
+                        }, 1800);
+                    }
+                } else {
                     imageTrack.style.transform = `translateX(${goRight()}px)`;
                     imageTrack.style.transition = `transform 0s ease 0s`;
+                    imageIndex += 1;
                 }
                 break;
             case 0:
-                if(isEffect) {
+                if(effect.includes("slide")) {
                     imageTrack.style.transform = `translateX(${goLeft()}px)`;
                     imageTrack.style.transition = `transform 0.8s ease 0s`;
-                } else if (!isEffect) {
+                    imageIndex -= 1;
+                } else if (effect.includes("fade")) {
+                    if(imageIndex > 0) {
+                        images[imageIndex].classList.toggle("slider__image--fade");
+                        window.setTimeout(function () {
+                            imageTrack.style.transform = `translateX(${goLeft()}px)`;
+                            imageTrack.style.transition = `transform 0s ease 0s`;
+                            imageIndex -= 1;
+                            images[imageIndex].classList.toggle("slider__image--fade");
+                        }, 1800);
+                    }
+                } else {
                     imageTrack.style.transform = `translateX(${goLeft()}px)`;
                     imageTrack.style.transition = `transform 0s ease 0s`;
+                    imageIndex -= 1;
                 }
                 break;
             default:
@@ -72,15 +98,34 @@ document.querySelectorAll(".button--effect").forEach((effectButton, index) => {
     effectButton.addEventListener("click", function() {
         switch(index) {
             case 0:
-                isEffect = true;
-                console.log("effects: ", isEffect)
+                effect = "slide";
+                images.forEach((image, index) => {
+                    if(image.classList.contains("slider__image--fade")) {   
+                        image.classList.remove("slider__image--fade");  
+                    }
+                });
+                console.log(effect);
                 break;
             case 1:
-                isEffect = false;
-                console.log("effects: ", isEffect)
+                effect = "fade";
+                console.log(imageIndex);
+                images.forEach((image, index) => {
+                    if(index != 0 && index != imageIndex) {   
+                        image.classList.toggle("slider__image--fade");  
+                    }
+                });
+                console.log(effect);
+                break;
+            case 2:
+                effect = "";
+                images.forEach((image, index) => {
+                    if(image.classList.contains("slider__image--fade")) {   
+                        image.classList.remove("slider__image--fade");  
+                    }
+                });
                 break;
             default:
-                isEffect = false;
+                effect = "";
                 break;
         }
     });
@@ -88,41 +133,45 @@ document.querySelectorAll(".button--effect").forEach((effectButton, index) => {
 
 document.addEventListener('keyup', function (event) {
     if(event.keyCode === 39) {
-        if(isEffect){
+        if(effect.includes("slide")){
             imageTrack.style.transform = `translateX(${goRight()}px)`;
             imageTrack.style.transition = `transform 0.8s ease 0s`;
-        } else if(!isEffect) {
+            imageIndex += 1;
+        } else if(effect.includes("fade")) {
+            if(imageIndex < 4) {
+                images[imageIndex].classList.toggle("slider__image--fade");
+                window.setTimeout(function () {
+                    imageTrack.style.transform = `translateX(${goRight()}px)`;
+                    imageTrack.style.transition = `transform 0s ease 0s`;
+                    imageIndex += 1;
+                    images[imageIndex].classList.toggle("slider__image--fade");
+                }, 1800);
+            }
+        } else {
             imageTrack.style.transform = `translateX(${goRight()}px)`;
             imageTrack.style.transition = `transform 0s ease 0s`;
+            imageIndex += 1;
         }
     }
     if(event.keyCode === 37) {
-        if(isEffect) {
+        if(effect.includes("slide")) {
             imageTrack.style.transform = `translateX(${goLeft()}px)`;
             imageTrack.style.transition = `transform 0.8s ease 0s`;
-        } else if(!isEffect) {
+            imageIndex -= 1;
+        } else if(effect.includes("fade")) {
+            if(imageIndex > 0) {
+                images[imageIndex].classList.toggle("slider__image--fade");
+                window.setTimeout(function () {
+                    imageTrack.style.transform = `translateX(${goLeft()}px)`;
+                    imageTrack.style.transition = `transform 0s ease 0s`;
+                    imageIndex -= 1;
+                    images[imageIndex].classList.toggle("slider__image--fade");
+                }, 1800);
+            }
+        } else {
             imageTrack.style.transform = `translateX(${goLeft()}px)`;
             imageTrack.style.transition = `transform 0s ease 0s`;
+            imageIndex -= 1;
         }
     }
 });
-
-/**
- * Tests wie CloneNode() Funktion angewendet wird.
-
-index 0 = left swipe
-index 1 = right swipe
-const images = document.querySelectorAll(".slider__image");
-const newImages = images.cloneNode(true);
-const newImageTrack = imageTrack.cloneNode(true);
-document.querySelector(".slider__container").appendChild(newImageTrack);
-console.log(newImages);
-function cloneImages() {
-    document.querySelectorAll(".slider__image").forEach((element, index) => {
-        // const newImage = element.cloneNode(true);
-        // imageTrack.removeChild(element);    
-        //imageTrack.appendChild(newImage);
-        
-    });
-} 
- */
